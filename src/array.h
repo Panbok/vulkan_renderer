38742338d4075e74a7c37e37f756f3e9b679f693
Yaroslav Panok <yaroslav_panok@epam.com>
2025-04-22 13:29:15 +0300

    feat(array): impl fixed array alloc on arena

---
+++ src/array.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "arena.h"
+
+#define ArrayConstructor(type, name)                                           \
+  struct Array_##name;                                                         \
+  typedef struct Array_##name {                                                \
+    Arena *arena;                                                              \
+    size_t stride;                                                             \
+    size_t length;                                                             \
+    type *data;                                                                \
+  } Array_##name;                                                              \
+                                                                               \
+  Array_##name array_create_##name(Arena *arena, const size_t length) {        \
+    assert(arena != NULL);                                                     \
+    assert(length > 0);                                                        \
+                                                                               \
+    Array_##name array = {arena, sizeof(type), length,                         \
+                          arena_alloc(arena, length * sizeof(type))};          \
+    return array;                                                              \
+  }                                                                            \
+                                                                               \
+  type *array_get_##name(const Array_##name *array, const size_t index) {      \
+    assert(array != NULL);                                                     \
+    assert(index < array->length);                                             \
+    return (type *)(array->data + index * array->stride);                      \
+  }                                                                            \
+                                                                               \
+  void array_set_##name(Array_##name *array, const size_t index, type value) { \
+    assert(array != NULL);                                                     \
+    assert(index < array->length);                                             \
+    array->data[index * array->stride] = value;                                \
+  }
+
+#define Array(type) ArrayConstructor(type, type)
+
+Array(uint8_t);
+
+Array(uint32_t);
+
+Array(uint64_t);
+
+Array(size_t);
