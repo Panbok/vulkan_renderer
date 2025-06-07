d3fd3d51ded5c3585f0acfb3b426621d4d21793f
Yaroslav Panok <yaroslav_panok@epam.com>
2025-06-07 16:56:10 +0300

    fix(bitset): fix asserts on flags range

--- lib/src/containers/bitset.c
+++ lib/src/containers/bitset.c
@@ -10,19 +10,22 @@ void bitset8_set(Bitset8 *bitset, uint8_t flag) {
 }
 
 void bitset8_clear(Bitset8 *bitset, uint8_t flag) {
-  assert(flag < 8 && "Flag must be less than 8");
+  assert((flag & (flag - 1)) == 0 && flag != 0 && flag <= 0x80 &&
+         "Flag must be a single power of 2 within 8-bit range");
   assert(bitset != NULL && "Bitset must not be NULL");
   bitset->set &= ~flag; // Use bitwise AND with the inverse of the flag
 }
 
 void bitset8_toggle(Bitset8 *bitset, uint8_t flag) {
-  assert(flag < 8 && "Flag must be less than 8");
+  assert((flag & (flag - 1)) == 0 && flag != 0 && flag <= 0x80 &&
+         "Flag must be a single power of 2 within 8-bit range");
   assert(bitset != NULL && "Bitset must not be NULL");
   bitset->set ^= flag; // Use bitwise XOR to toggle the flag
 }
 
 bool32_t bitset8_is_set(Bitset8 *bitset, uint8_t flag) {
-  assert(flag < 8 && "Flag must be less than 8");
+  assert((flag & (flag - 1)) == 0 && flag != 0 && flag <= 0x80 &&
+         "Flag must be a single power of 2 within 8-bit range");
   assert(bitset != NULL && "Bitset must not be NULL");
   return (bitset->set & flag) !=
          0; // Use bitwise AND to check if the flag is set
