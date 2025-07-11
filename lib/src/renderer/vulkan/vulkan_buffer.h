59bb993ff364fa847f4f568716a52d63b2c976e1
Yaroslav Panok <yaroslav_panok@epam.com>
2025-07-11 11:29:44 +0300

    feat(vulkan): specific buffer bindings & change params

--- lib/src/renderer/vulkan/vulkan_buffer.h
+++ lib/src/renderer/vulkan/vulkan_buffer.h
@@ -1,11 +1,11 @@
 #pragma once
 
+#include "vulkan_command.h"
 #include "vulkan_types.h"
 
-bool8_t vulkan_buffer_create(VulkanBackendState *state, uint64_t size,
-                             VkBufferUsageFlagBits usage,
-                             uint32_t memory_property_flags,
-                             bool8_t bind_on_create, VulkanBuffer *out_buffer);
+bool8_t vulkan_buffer_create(VulkanBackendState *state,
+                             const BufferDescription *desc,
+                             struct s_BufferHandle *out_buffer);
 
 void vulkan_buffer_destroy(VulkanBackendState *state, VulkanBuffer *buffer);
 
@@ -13,19 +13,36 @@ bool8_t vulkan_buffer_resize(VulkanBackendState *state, uint64_t new_size,
                              VulkanBuffer *buffer, VkQueue queue,
                              VkCommandPool pool);
 
-void vulkan_buffer_bind(VulkanBackendState *state, VulkanBuffer *buffer,
-                        uint64_t offset);
+bool8_t vulkan_buffer_bind(VulkanBackendState *state, VulkanBuffer *buffer,
+                           uint64_t offset);
+
+void vulkan_buffer_bind_vertex_buffers(VulkanBackendState *state,
+                                       VulkanCommandBuffer *command_buffer,
+                                       uint32_t first_binding,
+                                       uint32_t buffer_count,
+                                       const VkBuffer *buffers,
+                                       const VkDeviceSize *offsets);
+
+void vulkan_buffer_bind_vertex_buffer(VulkanBackendState *state,
+                                      VulkanCommandBuffer *command_buffer,
+                                      uint32_t binding, VkBuffer buffer,
+                                      VkDeviceSize offset);
+
+void vulkan_buffer_bind_index_buffer(VulkanBackendState *state,
+                                     VulkanCommandBuffer *command_buffer,
+                                     VkBuffer buffer, VkDeviceSize offset,
+                                     VkIndexType index_type);
 
 void *vulkan_buffer_lock_memory(VulkanBackendState *state, VulkanBuffer *buffer,
                                 uint64_t offset, uint64_t size, uint32_t flags);
-void vulkan_buffer_unlock_memory(VulkanBackendState *state,
-                                 VulkanBuffer *buffer);
+bool8_t vulkan_buffer_unlock_memory(VulkanBackendState *state,
+                                    VulkanBuffer *buffer);
 
-void vulkan_buffer_load_data(VulkanBackendState *state, VulkanBuffer *buffer,
-                             uint64_t offset, uint64_t size, uint32_t flags,
-                             const void *data);
+bool8_t vulkan_buffer_load_data(VulkanBackendState *state, VulkanBuffer *buffer,
+                                uint64_t offset, uint64_t size, uint32_t flags,
+                                const void *data);
 
-void vulkan_buffer_copy_to(VulkanBackendState *state, VkCommandPool pool,
-                           VkFence fence, VkQueue queue, VkBuffer source,
-                           uint64_t source_offset, VkBuffer dest,
-                           uint64_t dest_offset, uint64_t size);
+bool8_t vulkan_buffer_copy_to(VulkanBackendState *state,
+                              VulkanBuffer *buffer_handle, VkBuffer source,
+                              uint64_t source_offset, VkBuffer dest,
+                              uint64_t dest_offset, uint64_t size);
