61f9a818aca5adc145640c4e69ae22d3c3630c25
Yaroslav Panok <yaroslav_panok@epam.com>
2025-07-19 10:17:30 +0300

    feat(renderer): impl camera resource

---
+++ lib/src/renderer/resources/camera.c
@@ -0,0 +1,202 @@
+#include "camera.h"
+#include "math/math.h"
+
+void camera_perspective_create(Camera *camera, InputState *input_state,
+                               Window *window, float32_t target_frame_rate,
+                               float32_t zoom, float32_t near_clip,
+                               float32_t far_clip) {
+  assert_log(input_state != NULL, "Input state is NULL");
+  assert_log(window != NULL, "Window is NULL");
+
+  camera->input_state = input_state;
+  camera->target_frame_rate = target_frame_rate;
+  camera->window = window;
+
+  camera->type = CAMERA_TYPE_PERSPECTIVE;
+
+  camera->speed = 2.5f;
+  camera->sensitivity = 0.1f;
+  camera->yaw = -90.0f;
+  camera->pitch = 0.0f;
+
+  camera->position = vec3_new(0.0f, 0.0f, -5.0f);
+  camera->forward = vec3_new(0.0f, 0.0f, -6.0f);
+  camera->up = vec3_new(0.0f, 1.0f, 0.0f);
+  camera->right = vec3_new(1.0f, 0.0f, 0.0f);
+  camera->world_up = vec3_new(0.0f, 1.0f, 0.0f);
+
+  camera->near_clip = near_clip;
+  camera->far_clip = far_clip;
+  camera->zoom = zoom;
+
+  int8_t wheel_delta;
+  input_get_mouse_wheel(camera->input_state, &wheel_delta);
+  camera->previous_wheel_delta = wheel_delta;
+}
+
+void camera_orthographic_create(Camera *camera, InputState *input_state,
+                                Window *window, float32_t target_frame_rate,
+                                float32_t left, float32_t right,
+                                float32_t bottom, float32_t top,
+                                float32_t near_clip, float32_t far_clip) {
+  assert_log(input_state != NULL, "Input state is NULL");
+  assert_log(window != NULL, "Window is NULL");
+
+  camera->input_state = input_state;
+  camera->window = window;
+
+  camera->type = CAMERA_TYPE_ORTHOGRAPHIC;
+  camera->target_frame_rate = target_frame_rate;
+  camera->zoom = 1.0f;
+
+  camera->speed = 1.0f;
+  camera->sensitivity = 0.1f;
+  camera->yaw = -90.0f;
+  camera->pitch = 0.0f;
+
+  camera->position = vec3_new(0.0f, 0.0f, 0.0f);
+  camera->forward = vec3_new(0.0f, 0.0f, 1.0f);
+  camera->up = vec3_new(0.0f, 1.0f, 0.0f);
+  camera->right = vec3_new(1.0f, 0.0f, 0.0f);
+  camera->world_up = vec3_new(0.0f, 1.0f, 0.0f);
+
+  camera->near_clip = near_clip;
+  camera->far_clip = far_clip;
+  camera->left_clip = left;
+  camera->right_clip = right;
+  camera->bottom_clip = bottom;
+  camera->top_clip = top;
+}
+
+// todo: impl proper mouse capture with center warping (proper FPS camera)
+// todo: impl setting dirty flag for skipping view and projection matrix
+// recalculation
+void camera_update(Camera *camera, float32_t delta_time) {
+  assert_log(camera->input_state != NULL, "Input state is NULL");
+  assert_log(camera->type != CAMERA_TYPE_NONE, "Camera type is NONE");
+
+  if (input_is_key_down(camera->input_state, KEY_TAB)) {
+    window_set_mouse_capture(camera->window,
+                             !camera->window->is_mouse_captured);
+  }
+
+  float32_t velocity = camera->speed * delta_time;
+  if (input_is_key_down(camera->input_state, KEY_W)) {
+    camera->position =
+        vec3_sub(camera->position,
+                 vec3_scale(camera->forward, camera->speed * delta_time));
+  }
+
+  if (input_is_key_down(camera->input_state, KEY_S)) {
+    camera->position =
+        vec3_add(camera->position,
+                 vec3_scale(camera->forward, camera->speed * delta_time));
+  }
+
+  if (input_is_key_down(camera->input_state, KEY_A)) {
+    camera->position =
+        vec3_sub(camera->position,
+                 vec3_scale(camera->right, camera->speed * delta_time));
+  }
+
+  if (input_is_key_down(camera->input_state, KEY_D)) {
+    camera->position =
+        vec3_add(camera->position,
+                 vec3_scale(camera->right, camera->speed * delta_time));
+  }
+
+  int8_t wheel_delta;
+  input_get_mouse_wheel(camera->input_state, &wheel_delta);
+
+  if (wheel_delta != camera->previous_wheel_delta) {
+    camera->zoom -= wheel_delta * 0.1f;
+    if (camera->zoom < 1.0f)
+      camera->zoom = 1.0f;
+    if (camera->zoom > 45.0f)
+      camera->zoom = 45.0f;
+
+    camera->previous_wheel_delta = wheel_delta;
+  }
+
+  int32_t x, y;
+  input_get_mouse_position(camera->input_state, &x, &y);
+
+  int32_t last_x, last_y;
+  input_get_previous_mouse_position(camera->input_state, &last_x, &last_y);
+
+  if ((x == last_x && y == last_y) || (x == 0 && y == 0) ||
+      (last_x == 0 && last_y == 0)) {
+    return;
+  }
+
+  float32_t x_offset = x - last_x;
+  float32_t y_offset = last_y - y;
+
+  float32_t max_mouse_delta = MAX_MOUSE_DELTA / camera->sensitivity;
+  x_offset = clamp_f32(x_offset, -max_mouse_delta, max_mouse_delta);
+  y_offset = clamp_f32(y_offset, -max_mouse_delta, max_mouse_delta);
+
+  float32_t frame_adjusted_sensitivity =
+      camera->sensitivity * delta_time * camera->target_frame_rate;
+  camera->yaw -= x_offset * frame_adjusted_sensitivity;
+  camera->pitch += y_offset * frame_adjusted_sensitivity;
+
+  if (camera->pitch > 89.0f)
+    camera->pitch = 89.0f;
+  if (camera->pitch < -89.0f)
+    camera->pitch = -89.0f;
+
+  Vec3 front = vec3_new(
+      cos_f32(to_radians(camera->yaw)) * cos_f32(to_radians(camera->pitch)),
+      sin_f32(to_radians(camera->pitch)),
+      sin_f32(to_radians(camera->yaw)) * cos_f32(to_radians(camera->pitch)));
+  camera->forward = vec3_normalize(front);
+
+  camera->right = vec3_normalize(vec3_cross(camera->forward, camera->world_up));
+  camera->up = vec3_normalize(vec3_cross(camera->right, camera->forward));
+}
+
+Mat4 camera_get_view_matrix(Camera *camera) {
+  assert_log(camera != NULL, "Camera is NULL");
+  assert_log(camera->type != CAMERA_TYPE_NONE, "Camera type is NONE");
+
+  return mat4_look_at(camera->position,
+                      vec3_add(camera->position, camera->forward), camera->up);
+}
+
+Mat4 camera_get_projection_matrix(Camera *camera) {
+  assert_log(camera != NULL, "Camera is NULL");
+  assert_log(camera->type != CAMERA_TYPE_NONE, "Camera type is NONE");
+
+  if (camera->type == CAMERA_TYPE_PERSPECTIVE) {
+    WindowPixelSize window_size = window_get_pixel_size(camera->window);
+    return mat4_perspective(to_radians(camera->zoom),
+                            (float32_t)window_size.width /
+                                (float32_t)window_size.height,
+                            camera->near_clip, camera->far_clip);
+  }
+
+  if (camera->type == CAMERA_TYPE_ORTHOGRAPHIC) {
+    return mat4_ortho(camera->left_clip, camera->right_clip,
+                      camera->bottom_clip, camera->top_clip, camera->near_clip,
+                      camera->far_clip);
+  }
+
+  return mat4_identity();
+}
+
+void camera_destroy(Camera *camera) {
+  camera->type = CAMERA_TYPE_NONE;
+  camera->input_state = NULL;
+  camera->window = NULL;
+  camera->zoom = 0.0f;
+  camera->speed = 0.0f;
+  camera->sensitivity = 0.0f;
+  camera->yaw = 0.0f;
+  camera->pitch = 0.0f;
+  camera->position = vec3_zero();
+  camera->forward = vec3_zero();
+  camera->up = vec3_zero();
+  camera->right = vec3_zero();
+  camera->world_up = vec3_zero();
+}
---
+++ lib/src/renderer/resources/camera.h
@@ -0,0 +1,66 @@
+#pragma once
+
+#include "core/input.h"
+#include "math/mat.h"
+#include "math/math.h"
+#include "math/vec.h"
+#include "platform/window.h"
+
+#define MAX_MOUSE_DELTA 100.0f
+
+typedef enum CameraType {
+  CAMERA_TYPE_NONE,
+  CAMERA_TYPE_PERSPECTIVE,
+  CAMERA_TYPE_ORTHOGRAPHIC,
+} CameraType;
+
+typedef struct Camera {
+  InputState *input_state;
+  Window *window;
+  float32_t target_frame_rate;
+
+  CameraType type;
+
+  Vec3 position;
+  Vec3 forward;
+  Vec3 up;
+  Vec3 right;
+  Vec3 world_up;
+
+  float32_t yaw;
+  float32_t pitch;
+
+  float32_t speed;
+  float32_t sensitivity;
+
+  float32_t near_clip;
+  float32_t far_clip;
+
+  // Perspective
+  float32_t zoom;
+  float32_t previous_wheel_delta;
+
+  // Orthographic
+  float32_t left_clip;
+  float32_t right_clip;
+  float32_t bottom_clip;
+  float32_t top_clip;
+} Camera;
+
+void camera_perspective_create(Camera *camera, InputState *input_state,
+                               Window *window, float32_t target_frame_rate,
+                               float32_t fov, float32_t near_clip,
+                               float32_t far_clip);
+void camera_orthographic_create(Camera *camera, InputState *input_state,
+                                Window *window, float32_t target_frame_rate,
+                                float32_t left, float32_t right,
+                                float32_t bottom, float32_t top,
+                                float32_t near_clip, float32_t far_clip);
+
+void camera_update(Camera *camera, float32_t delta_time);
+
+Mat4 camera_get_view_matrix(Camera *camera);
+
+Mat4 camera_get_projection_matrix(Camera *camera);
+
+void camera_destroy(Camera *camera);
