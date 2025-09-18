92ff7d28355f820a7eab06175b2cc9bf2258da58
Yaroslav Panok <yaroslav_panok@epam.com>
2025-09-18 21:40:08 +0300

    ref(clock): update clock tests

--- tests/src/clock_test.c
+++ tests/src/clock_test.c
@@ -2,7 +2,7 @@
 
 static void test_clock_create(void) {
   printf("  Running test_clock_create...\n");
-  Clock clock = clock_create();
+  VkrClock clock = vkr_clock_create();
   assert(clock.start_time == 0.000000 && "Clock start time is not 0.0");
   assert(clock.elapsed == 0.000000 && "Clock elapsed time is not 0.0");
   printf("  test_clock_create PASSED\n");
@@ -10,37 +10,37 @@ static void test_clock_create(void) {
 
 static void test_clock_start(void) {
   printf("  Running test_clock_start...\n");
-  Clock clock = clock_create();
-  clock_start(&clock);
+  VkrClock clock = vkr_clock_create();
+  vkr_clock_start(&clock);
   assert(clock.start_time != 0.000000 && "Clock start time is 0.0");
   printf("  test_clock_start PASSED\n");
 }
 
 static void test_clock_stop(void) {
   printf("  Running test_clock_stop...\n");
-  Clock clock = clock_create();
-  clock_start(&clock);
+  VkrClock clock = vkr_clock_create();
+  vkr_clock_start(&clock);
   vkr_platform_sleep(1);
-  clock_update(&clock);
-  clock_stop(&clock);
+  vkr_clock_update(&clock);
+  vkr_clock_stop(&clock);
   assert(clock.elapsed != 0.000000 && "Clock elapsed time is 0.0");
   printf("  test_clock_stop PASSED\n");
 }
 
 static void test_clock_update_zero_start_time(void) {
   printf("  Running test_clock_update_zero_start_time...\n");
-  Clock clock = clock_create();
-  clock_update(&clock);
+  VkrClock clock = vkr_clock_create();
+  vkr_clock_update(&clock);
   assert(clock.elapsed == 0.000000 && "Clock elapsed time is not 0.0");
   printf("  test_clock_update_zero_start_time PASSED\n");
 }
 
 static void test_clock_update_non_zero_start_time(void) {
   printf("  Running test_clock_update_non_zero_start_time...\n");
-  Clock clock = clock_create();
-  clock_start(&clock);
+  VkrClock clock = vkr_clock_create();
+  vkr_clock_start(&clock);
   vkr_platform_sleep(1);
-  clock_update(&clock);
+  vkr_clock_update(&clock);
   assert(clock.elapsed != 0.000000 && "Clock elapsed time is 0.0");
   printf("  test_clock_update_non_zero_start_time PASSED\n");
 }
--- tests/src/clock_test.h
+++ tests/src/clock_test.h
@@ -1,5 +1,5 @@
 #pragma once
 
-#include "core/clock.h"
+#include "core/vkr_clock.h"
 
 bool32_t run_clock_tests(void);
