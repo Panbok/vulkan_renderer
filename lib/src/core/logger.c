d7af4f90aba3788dc96783c2fed4cf14465eafd2
Yaroslav Panok <yaroslav_panok@epam.com>
2025-12-04 13:29:28 +0200

    ref(logger): transition from arena-based memory management to vkr_allocator for logging functionality

--- lib/src/core/logger.c
+++ lib/src/core/logger.c
@@ -1,6 +1,7 @@
 #include "logger.h"
 
 vkr_global Arena *_log_arena = NULL;
+vkr_global VkrAllocator _log_allocator = {0};
 
 vkr_global const char *LOG_LEVELS[6] = {
     "[FATAL]: ", "[ERROR]: ", "[WARN]: ", "[INFO]: ", "[DEBUG]: ", "[TRACE]: "};
@@ -8,22 +9,28 @@ vkr_global const char *LOG_LEVELS[6] = {
 void log_init(Arena *arena) {
   assert(arena != NULL && "Log arena is not initialized.");
   _log_arena = arena;
+  _log_allocator.ctx = arena;
+  vkr_allocator_arena(&_log_allocator);
 }
 
 void _log_message(LogLevel level, const char *file, uint32_t line,
                   const char *fmt, ...) {
   assert(_log_arena != NULL && "Log arena is not initialized.");
 
-  Scratch scratch = scratch_create(_log_arena);
+  VkrAllocatorScope scope =
+      vkr_allocator_begin_scope(&_log_allocator); // scoped temp strings
+  if (!vkr_allocator_scope_is_valid(&scope)) {
+    return;
+  }
   va_list args;
   va_start(args, fmt);
-  String8 message = string8_create_formatted_v(_log_arena, fmt, args);
+  String8 message = string8_create_formatted_v(&_log_allocator, fmt, args);
   va_end(args);
 
   assert(message.str != NULL && "Error formatting log message.");
 
   String8 formatted_message = string8_create_formatted(
-      scratch.arena, "%s(%s:%d) %.*s\n", LOG_LEVELS[level], file, line,
+      &_log_allocator, "%s(%s:%d) %.*s\n", LOG_LEVELS[level], file, line,
       message.length, message.str);
 
   assert(formatted_message.str != NULL &&
@@ -35,5 +42,5 @@ void _log_message(LogLevel level, const char *file, uint32_t line,
     debug_break();
   }
 
-  scratch_destroy(scratch, ARENA_MEMORY_TAG_STRING);
+  vkr_allocator_end_scope(&scope, VKR_ALLOCATOR_MEMORY_TAG_STRING);
 }
