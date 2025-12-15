7a104236fb6bf36e066d4fe491f815ce48071fcb
Yaroslav Panok <yaroslav_panok@epam.com>
2025-12-15 11:43:20 +0200

    feat(tests): add allocator test header for unit testing memory allocation functionalities

---
+++ tests/src/allocator_test.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "core/vkr_threads.h"
+#include "defines.h"
+#include "memory/arena.h"
+#include "memory/vkr_allocator.h"
+#include "memory/vkr_arena_allocator.h"
+#include "memory/vkr_dmemory.h"
+#include "memory/vkr_dmemory_allocator.h"
+
+bool32_t run_allocator_tests(void);
